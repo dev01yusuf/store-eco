38ac0e8f624e4812086c86b6d09f44f12ac8195c
dev01yusuf <taoufikallahy@gmail.com>
2022-08-08 18:00:05 +0100

    add filter categories of all products

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Skeleton from 'react-loading-skeleton';
 
 function Products() {
     const [data, setData] = useState([]);
@@ -27,19 +28,45 @@ function Products() {
     const Loading = () => {
         return (
             <>
-                Loading ...
+                <div className="col-md-3">
+                    <Skeleton height={350} />
+                </div>
+                <div className="col-md-3">
+                    <Skeleton height={350} />
+                </div>
+                <div className="col-md-3">
+                    <Skeleton height={350} />
+                </div>
+                <div className="col-md-3">
+                    <Skeleton height={350} />
+                </div>
             </>
-        )
+        );
+    };
+    const filterProduct = (cat) => {
+        const updateList = data.filter((x)=>x.category === cat);
+        setFilter(updateList);
     }
     const ShowProducts = () => {
         return (
             <>
                 <div className="bottons d-flex justify-content-center mb-5 pb-5">
-                    <button className="btn btn-outline-dark me-2"> All </button>
-                    <button className="btn btn-outline-dark me-2"> Men's Clothing  </button>
-                    <button className="btn btn-outline-dark me-2"> Women's Clothing  </button>
-                    <button className="btn btn-outline-dark me-2"> Jewelery </button>
-                    <button className="btn btn-outline-dark me-2"> Electronic </button>
+                    <button className="btn btn-outline-dark me-2"
+                        onClick={() => setFilter(data)}
+                    > All </button>
+                    <button className="btn btn-outline-dark me-2"
+                        onClick={() => filterProduct("men's clothing")}>
+                        Men's Clothing
+                    </button>
+                    <button className="btn btn-outline-dark me-2"
+                        onClick={() => filterProduct("women's clothing")}
+                    > Women's Clothing  </button>
+                    <button className="btn btn-outline-dark me-2"
+                        onClick={() => filterProduct("jewelery")}
+                    > Jewelery </button>
+                    <button className="btn btn-outline-dark me-2"
+                        onClick={() => filterProduct("electronics")}
+                    > Electronic </button>
                 </div>
 
                 {filter.map((product) => {
@@ -48,13 +75,13 @@ function Products() {
                             <div className='col-md-3 mb-4'>
                                 <div class="card h-100 text-center p-4" key={product.id}>
                                     <img src={product.image} class="card-img-top" alt={product.title} height="250px" />
-                                        <div class="card-body">
-                                            <h5 class="card-title mb-0">{product.title.substring(0,12)}...</h5>
-                                            <p class="card-text lead fw-bold">
-                                                ${product.price}
-                                            </p>
-                                            <a href="#" class="btn btn-outline-dark">Buy Now</a>
-                                        </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title mb-0">{product.title.substring(0, 12)}...</h5>
+                                        <p class="card-text lead fw-bold">
+                                            ${product.price}
+                                        </p>
+                                        <a href="#" class="btn btn-outline-dark">Buy Now</a>
+                                    </div>
                                 </div>
                             </div>
                         </>
