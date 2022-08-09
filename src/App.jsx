eea09861e1dd04daf7306d28d6822530cd2e22af
dev01yusuf <taoufikallahy@gmail.com>
2022-08-09 11:23:42 +0100

    create navlink with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Navbar from './component/Navbar'
 import Home from './component/Home'
-
+import {Routes, Route} from 'react-router-dom'
+import Products from './component/Products'
 
 function App() {
 
   return (
     <>
     <Navbar />
-   <Home />
+    <Routes>
+      <Route  path='/' element={<Home/>} />
+      <Route  path='/products' element={<Products/>} />
+    </Routes>
     </>
    
   )
